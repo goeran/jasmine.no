1f034fdd70340bfc81391e99025b8a00e91742a8
goeran <mail@goeran.no>
2012-09-14 16:14:22 +0200

    added script file

---
+++ jasmine.no.js
@@ -0,0 +1,18 @@
+﻿/*
+    Utvider jasmine spec dsl til å støtte norsk språk.
+    Dette gjør at det er mulig å skrive jasmine specene på norsk.
+*/
+
+//describe
+var beskriv = describe;
+var xbeskriv = xdescribe;
+var når = describe;
+var xnår = xdescribe;
+
+//it
+var da = it;
+var xda = xit;
+
+//beforeEach
+førHverTest = beforeEach;
+etterHverTest = afterEach;
